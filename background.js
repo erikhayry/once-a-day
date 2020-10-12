fbf3c91825d17e112272360c8c02c1cd0b07c7de
Erik Häyry <erik.portin@svt.se>
2020-10-12 14:28:55 +0200

    use search to retrieve history

--- background.js
+++ background.js
@@ -3,43 +3,10 @@ Sentry.init({ dsn: 'https://dd2362f7d005446585e6414b1662594e@sentry.io/1407701' 
 Sentry.configureScope((scope) => {
     scope.setTag("version", VERSION);
 });
+const MINUTE = 1000 * 60;
+const ALLOWED_BROWSING_TIME_IN_MINUTES = 20 * MINUTE;
 
-const ALLOWED_BROWSING_TIME_IN_MINUTES = 20;
-
-function visitsTodayFilter(visit) {
-    const visitTime = visit.visitTime;
-    return moment(visitTime).dayOfYear() === moment().dayOfYear()
-}
-
-function findEarlier(el, index, arr){
-    const nextEl = arr[index + 1];
-    const visitTime = el.visitTime;
-    if(nextEl){
-        const diff = moment(nextEl.visitTime).diff(moment(visitTime), 'minutes');
-
-        return diff > ALLOWED_BROWSING_TIME_IN_MINUTES;
-    }
-
-    return false;
-}
-
-function handleVisits(visits = []) {
-    const visitsToday = visits.filter(visitsTodayFilter);
-    const earlierVisit = visitsToday.find(findEarlier);
-
-    if(earlierVisit){
-        const visitTime = earlierVisit.visitTime;
-        return {
-            lastVisit: visitTime,
-            isVisitedToday: true,
-            url: browser.runtime.getURL('/dist/landing.html')
-        };
-    }
-
-    return { isVisitedToday: false }
-}
-
-async function listVisits() {
+async function handleVisits() {
     const url = await getUrl();
     const allowList = await getAllowList();
     const urlIsAllowed = url && allowList.some(allowListItem => url.indexOf(allowListItem) > -1);
@@ -49,15 +16,28 @@ async function listVisits() {
     }
 
     const origin = new URL(url).origin;
-    const originVisits = await browser.history.getVisits({
-        url: origin
+    const offset = new Date().getTimezoneOffset() * MINUTE;
+    const startTime = new Date().setHours(0,0,0,0) + offset;
+    const endTime = new Date().getTime() - ALLOWED_BROWSING_TIME_IN_MINUTES + offset;
+    const originVisitsSearch = await browser.history.search({
+        text: origin,
+        startTime,
+        endTime
     });
-    const urlVisits = await browser.history.getVisits({
-        url
-    });
-    const visits = originVisits.concat(urlVisits);
 
-    return handleVisits(visits)
+    const visitsWithSameOrigin = originVisitsSearch
+        .filter(visit => new URL(visit.url).origin === origin)
+        .sort((a, b) => a.lastVisitTime - b.lastVisitTime)
+
+    if(visitsWithSameOrigin.length > 0){
+        return {
+            lastVisit: visitsWithSameOrigin[0].lastVisitTime,
+            isVisitedToday: true,
+            url: browser.runtime.getURL('/dist/landing.html')
+        };
+    }
+
+    return { isVisitedToday: false }
 }
 
 async function getAllowList(){
@@ -84,7 +64,7 @@ async function getUrl(){
 
 async function checkHistory() {
     try {
-        return await listVisits();
+        return await handleVisits();
     } catch(e){
         console.error(e)
     }
